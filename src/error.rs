8fc581677a0ffe3ed9d1f6f976e29c9d2c320197
khuongduy354 <42113313+khuongduy354@users.noreply.github.com>
2023-03-04 16:02:55 +0700

    feat:error handler

--- src/error.rs
+++ src/error.rs
@@ -1,14 +1,25 @@
-use std::fmt::Error;
+use std::{error::Error, fmt::Display};
 
+#[derive(Debug)]
 pub enum HectaError {
-    FileIO,
+    IOError(std::io::Error),
     InvalidOperation(String),
     InvalidCursorPos(String),
-    StandardError(Error),
+    // StandardError(std::io::Error),
 }
 
-impl From<Error> for HectaError {
-    fn from(e: Error) -> Self {
-        HectaError::StandardError(e)
+impl Error for HectaError {
+    // fn from(e: Error) -> Self {
+    //     HectaError::StandardError(e)
+    // }
+}
+impl From<std::io::Error> for HectaError {
+    fn from(e: std::io::Error) -> Self {
+        HectaError::IOError(e)
+    }
+}
+impl Display for HectaError {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        write!(f, "{}", self)
     }
 }
